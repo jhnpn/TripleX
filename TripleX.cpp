2573e06a7284d3b03d0a13e64490136431a7e4e2
jhnpn <j.hnpn123@gmail.com>
2021-02-05 18:31:19 -0800

    cleaned up code by adding new functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
-    //Prints welcome message to the terminal
-    std::cout << "You're an adventurer entering a dungeon that consists of many rooms full of puzzles...";
-    std::cout << std::endl; //creates a new line
-    std::cout << "You will need to solve them in order to continue." << std::endl;
+    // Prints welcome message to the terminal
+    std::cout << "You're an adventurer entering a dungeon that consists of many rooms full of puzzles...\n";
+    std::cout << "You will need to solve them in order to continue.\n\n";
+}
 
-    //declares 3 number code
-    const int CodeA = 4; //example of a variable
+void PlayGame()
+{
+    PrintIntroduction();
+
+    // declares 3 number code
+    const int CodeA = 4;
     const int CodeB = 8;
-    const int CodeC = 2;
+    const int CodeC = 2; 
 
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
 
-    //prints sum and product to the terminal
-    std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl; //calls out the variable
-    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+    // prints sum and product to the terminal
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
-    //gets character input from the terminal
+    // store player guess
     int GuessA, GuessB, GuessC;
-
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // check if the player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
     {
-        std::cout << "You may advance to the next room.";
+        std::cout << "\nYou may advance to the next room.";
     }
     else
     {
-        std::cout << "There seems to be an error in your answer...";
+        std::cout << "\nThere seems to be an error in your answer...";
     }
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
